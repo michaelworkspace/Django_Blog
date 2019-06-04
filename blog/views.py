55f62bb4a831097c3df212d4bf3575c37b227f0f
Michael Le <you@example.com>
2019-06-04 09:21:49 -0500

    blog commit

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Michael Le',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'July 15, 2019'
+    },
+    {
+        'author': 'Phucanese Le',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'July 16, 2019'
+    }
+]
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    return render(request, 'blog/home.html')
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
