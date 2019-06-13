11fff665aebbbe2c2378e6e0be24f9f531774846
Michael Le <you@example.com>
2019-06-13 15:04:35 -0500

    template commit

--- blog/views.py
+++ blog/views.py
@@ -17,8 +17,11 @@ posts = [
 
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
